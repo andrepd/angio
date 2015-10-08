066a2fa4c30b9725263d084c646e4ee55e242d6f
andrepd <andrepduarte@gmail.com>
2015-10-08 15:46:38 +0100

    akjgga

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from os import system
 from sys import argv, exit
 from time import clock,time
 
-defines = {
+defaults = {
         'Lx': '128',
         'Ly': '128',
         'medini': '-.2',
@@ -18,58 +18,72 @@ defines = {
         'tf': '100000',
 }
 
-if len(argv) != 2:
-    print 'Usage:',argv[0],'file'
-    exit()
-
 def box(s,c='*',n=None):
     if n == None:
         n = max([len(x) for x in s])+6
     return c*n+'\n' + '\n'.join([c+x.center(n-2)+c for x in s]) + '\n'+c*n
 
-print box([
-'Computational Biology',
-'University of Coimbra - Portugal',
-' ',
-'Sprouting Angiogenesis',
-' ',
-'Phase-field model with elasticity',
-' ',
-'Developers:',
-'Rui Travasso, PhD',
-'Andre Duarte, BSc',
-'Marcos Gouveia, BSc',
-'Marina Oliveira, Student'
-])
+def parse(name):
+    print 'Reading input file...',
+    defines = defaults
+    tips = []
+    with open(name, 'r') as f:
+        f = [x for x in f if x[0] != '#' and x != '\n' and x != 'TIPS\n']
+        for i in f:
+            if i[0] == '\t':
+                tips.append(i[1:])
+            else:
+                x = i.strip(' ').strip('\t').strip('\n').split('=')
+                defines[x[0]] = x[1]
+    print 'Done.'
+    return defines,tips
 
-print 'Reading input file...\n'
+def build(defines, tips):
+    if not tips:
+        tips.append(str(int(defines['Lx'])/5+10)+' '+str(int(defines['Ly'])/2))
+    with open('tips.in','w') as f:
+        for i in tips:
+            f.write(i)
 
-tips = []
-with open(argv[1], 'r') as f:
-    f = [x for x in f if x[0] != '#' and x != '\n' and x != 'TIPS\n']
-    for i in f:
-        if i[0] == '\t':
-            tips.append(i[1:])
-        else:
-            x = i.strip(' ').strip('\t').strip('\n').split('=')
-            defines[x[0]] = x[1]
+    print 'Compiling...',
+    system('ulimit -s '+str(int(defines['Lx'])*int(defines['Ly'])*128/1024))
+    system('g++ -pg main.cpp -std=c++11 -march=native -O3 -o main '+' '.join(['-D'+i+'='+j for i,j in defines.items()]))
+    print 'Done.'
 
-if not tips:
-    tips.append(str(int(defines['Lx'])/5+10)+' '+str(int(defines['Ly'])/2))
-with open('tips.in','w') as f:
-    for i in tips:
-        f.write(i)
+def run():
+    if not os.path.exists('data'):
+        os.makedirs('data')
+    print 'Running:\n'
+    print '-----\n'
+    ti = time()
+    system('./main')
+    tf = time()
+    print '\n-----\n'
+    print 'Running time:',round(tf-ti,2),'seconds.'
+    print 'Done.'
 
-print 'Compiling... ',
-system('ulimit -s '+str(int(defines['Lx'])*int(defines['Ly'])*128/1024))
-system('g++ -pg main.cpp -std=c++11 -march=native -O3 -o main '+' '.join(['-D'+i+'='+j for i,j in defines.items()]))
-print 'Done.'
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print 'Usage:',argv[0],'file'
+        exit()
 
-print 'Running:\n'
-print '-----\n'
-ti = time()
-system('./main')
-tf = time()
-print '\n-----\n'
-print 'Running time:',round(tf-ti,2),'seconds.'
-print 'Done.'
+    print box([
+    'Computational Biology',
+    'University of Coimbra - Portugal',
+    ' ',
+    'Sprouting Angiogenesis',
+    ' ',
+    'Phase-field model with elasticity',
+    ' ',
+    'Developers:',
+    'Rui Travasso, PhD',
+    'Andre Duarte, BSc',
+    'Marcos Gouveia, BSc',
+    'Marina Oliveira, Student'
+    ])
+
+    d,t = parse(argv[1])
+    build(d,t)
+    run()
+
+
