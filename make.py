c2fbf3bf83e91c506fe493ecb9f9523aafccf457
andrepd <andrepduarte@gmail.com>
2015-11-20 15:14:31 +0000

    askdgba

--- make.py
+++ make.py
@@ -13,6 +13,8 @@ def frange(start, stop, step):
 pre = '/home/angio/samm/angio/'
 with open('tasks.txt','w') as f:
     for n,i in zip(count(), frange(0.2,1.2,0.05)):
+        if not os.path.exists(str(i)):
+            os.makedirs(str(i))
         comp = 'g++ main.cpp -std=c++11 -O3 -march=native -o main{0} -Doutput=\'\"{1}\"\' -DEecm={1}'.format(n,i)
         print comp
         os.system(comp)
