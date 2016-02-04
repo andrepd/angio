fd88ed8a99b69b93d017d3c5acc1137e91dd9f28
andrepd <andrepduarte@gmail.com>
2016-02-04 02:57:57 +0000

    make done

--- make.py
+++ make.py
@@ -29,8 +29,7 @@ pwd = os.getcwd()
 gcc = 'g++ main.cpp -std=c++11 -O3 -march=native -o main '
 stuff = ['main.cpp', 'srj.hpp', 'run.sh']
 
-# Criar Run #N e mover para lá stuff
-
+# Criar pasta Run #N
 N = 0
 for i in count(1):
     if not os.path.exists('Run #{}'.format(i)):
@@ -38,22 +37,27 @@ for i in count(1):
         N = i
         break
 
+# Mover stuff para lá
 for i in stuff:
     os.system('cp {} Run\\ #{}'.format(i,N))
 os.chdir('./Run #{}'.format(N))
 print('Starting Run #{}'.format(N))
 
+# Escrever cenas
 with open('tasks.txt','w') as f:
     for i,d in zip(count(1), permutations(consts)):
+        print('{}: '.format(i) + ', '.join('{} = {}'.format(a,b) for a,b in d.items()))
+
         #print(d)
         os.makedirs(str(i))
         comp = gcc + ' '.join('-{}={}'.format(a,b) for a,b in d.items())
         #print(comp)
         os.system(comp)
+        
         with open('consts.txt', 'w') as g:
             g.write('\n'.join('{} = {}'.format(a,b) for a,b in d.items()))
-        f.write('./{}/main\n'.format(i))
+        f.write('./{}/main >out.txt 2>err.txt\n'.format(i))
+        
         os.system('mv main consts.txt {}'.format(i))
         os.system('cp ../plot.gp {}'.format(i))
         os.system('mkdir {}/data'.format(i))
-        print('{}: '.format(i) + ', '.join('{} = {}'.format(a,b) for a,b in d.items()))
--- plot.gp
+++ plot.gp
@@ -7,7 +7,7 @@ unset key
 load '../colors.gp'
 
 set palette negative
-do for [i=1000:100000:200] {
+do for [i=1000:250000:200] {
 	set title sprintf("t = %d",i)
 	plot './data/aout_'.i matrix w image, './data/tout_'.i ps 4 lc 2
 	pause 0.03
